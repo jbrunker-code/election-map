9f7eba587e2a669378e6aa4e5013ac26c80f6af9
jennifer_b <jennifer.brunker@gmail.com>
2021-03-31 15:28:36 -0400

    Fixed html connection to javascript, changed names

--- script.js
+++ script.js
@@ -16,36 +16,36 @@ function makePolitician(name, partyColor){
   return politician;
 }/* makes politician factory function with party color, name, and method to tally votes  */
 
-let aoc = makePolitician("AOC", [132, 17, 11]);
-let bernie = makePolitician("Bernie", [245, 141, 136]);
+let knope = makePolitician("Leslie Knope", [132, 17, 11]);
+let meyers = makePolitician("Selena Meyers", [245, 141, 136]);
 
 /*creates my 2 politicians */
 
-aoc.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
-bernie.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+knope.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+meyers.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
 /*gives them results*/
 
-aoc.electionResults[9] = 1;
-bernie.electionResults[9] = 28;
-aoc.electionResults[4] = 17;
-bernie.electionResults[4] = 38;
-aoc.electionResults[43] = 11;
-bernie.electionResults[43] = 27;
+knope.electionResults[9] = 1;
+meyers.electionResults[9] = 28;
+knope.electionResults[4] = 17;
+meyers.electionResults[4] = 38;
+knope.electionResults[43] = 11;
+meyers.electionResults[43] = 27;
 
 /*updates the results*/
 
-aoc.tallyVotes();
-bernie.tallyVotes();
+knope.tallyVotes();
+meyers.tallyVotes();
 
 /* calls the vote tally function based on current results, updates total votes for each politician */
 
 let overallWinner = "";
-if (aoc.totalVotes < bernie.totalVotes){
-  overallWinner = "AOC";
+if (knope.totalVotes < meyers.totalVotes){
+  overallWinner = "Leslie Knope";
 }
-if (aoc.totalVotes < bernie.totalVotes){
-  overallWinner = "Bernie"
+if (knope.totalVotes < meyers.totalVotes){
+  overallWinner = "Selena Meyers"
 }
 else {
   overallWinner = "Draw"
@@ -55,10 +55,10 @@ else {
 
 function setStateResults (state) {
   theStates[state].winner = null;
-  if (aoc.electionResults[state] > bernie.electionResults[state]){
-    theStates[state].winner = aoc;
-  } else if (aoc.electionResults[state] < bernie.electionResults[state]){
-    theStates[state].winner = bernie;
+  if (knope.electionResults[state] > meyers.electionResults[state]){
+    theStates[state].winner = knope;
+  } else if (knope.electionResults[state] < meyers.electionResults[state]){
+    theStates[state].winner = meyers;
   }
 
   let stateWinner = theStates[state].winner;
@@ -74,18 +74,18 @@ function setStateResults (state) {
   let body = stateInfoTable.children[1];
   let stateName = header.children[0].children[0];
   let stateAbbrev = header.children[0].children[1];
-  let aocName = body.children[0].children[0];
-  let aocResults = body.children[0].children[1];
-  let bernieName = body.children[1].children[0];
-  let bernieResults = body.children[1].children[1];
+  let knopeName = body.children[0].children[0];
+  let knopeResults = body.children[0].children[1];
+  let meyersName = body.children[1].children[0];
+  let meyersResults = body.children[1].children[1];
   let winnerName = body.children[2].children[1];
 
   stateName.innerText = theStates[state].nameFull;
   stateAbbrev.innerText = "(" +theStates[state].nameAbbrev + ")";
-  aocName.innerText = aoc.name;
-  aocResults.innerText = aoc.electionResults[state];
-  bernieName.innerText = bernie.name;
-  bernieResults.innerText = bernie.electionResults[state];
+  knopeName.innerText = knope.name;
+  knopeResults.innerText = knope.electionResults[state];
+  meyersName.innerText = meyers.name;
+  meyersResults.innerText = meyers.electionResults[state];
 
   if (theStates[state].winner === null){
     winnerName.innerText = "DRAW"
@@ -100,10 +100,10 @@ function setStateResults (state) {
 let countryInfoTable = document.getElementById("countryResults");
 let row = countryInfoTable.children[0].children[0];
 
-row.children[0].innerText = aoc.name;
-row.children[1].innerText = aoc.totalVotes;
-row.children[2].innerText = bernie.name;
-row.children[3].innerText = bernie.totalVotes;
+row.children[0].innerText = knope.name;
+row.children[1].innerText = knope.totalVotes;
+row.children[2].innerText = meyers.name;
+row.children[3].innerText = meyers.totalVotes;
 row.children[5].innerText = overallWinner;
 
 /* populates top table with Overall Winner info */
